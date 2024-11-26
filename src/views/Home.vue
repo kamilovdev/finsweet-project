1583cc1bb8d160512f5aff921ae9935a82d43168
kamilovdev <amirbekkamilov737@gmail.com>
2024-11-26 17:43:52 +0500

    fix: edited image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -117,7 +117,7 @@
         <div class="services-list">
           <div class="service-item">
             <span class="service-icon">
-              <img src="../../public/section/icon-div-1.svg" alt="">
+              <img src="../../public/section/icon-div1.svg" alt="">
             </span>
             <div class="service-description">
               <h1>Technical support</h1>
@@ -141,7 +141,7 @@
 
           <div class="service-item">
             <span class="service-icon">
-              <img src="../../public/section/icon-div-2.svg" alt="">
+              <img src="../../public/section/icon-div2.svg" alt="">
             </span>
             <div class="service-description">
               <h1>Development</h1>
